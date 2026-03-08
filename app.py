800c174c1421330d331465bc71f5e4f133d452b6
Ezekiel Zaldivar <pipzaldivar@gmail.com>
2026-03-08 13:21:30 -0700

    Connect Streamlit frontend to QUESTIONS data

--- app.py
+++ app.py
@@ -1,4 +1,18 @@
 import streamlit as st
+from questionnaire import QUESTIONS
 
 st.title("Climate Impact Advisor")
-st.write("Test page")
+
+question = QUESTIONS["transport"]
+
+prompt = question["prompt"]
+options = question["options"]
+
+labels = []
+for choice in options:
+    value, label = options[choice]
+    labels.append(label)
+
+selected_label = st.radio(prompt, labels)
+
+st.write("You selected:", selected_label)
